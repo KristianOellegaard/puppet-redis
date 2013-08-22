1bf0a3eb2916dc3446b7cdecea75d5e9ef38045b
Kristian Øllegaard <kristian@kristian.io>
2013-08-22 14:18:05 +0200

    Don't let the regular redis start

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,11 @@
 class redis::install {
 
   package { 'redis-server': ensure => present }
+  service { "redis-server":
+    ensure     => stopped,
+    enable     => false,
+    hasrestart => true,
+    hasstatus  => true,
+  }
 
 }
